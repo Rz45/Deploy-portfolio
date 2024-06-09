68433f0157f900fc2db11a78101e232ba6e6083c
Rizky Aris Adzin <rizkyarisadzin@students.amikom.ac.id>
2024-06-09 23:13:50 +0700

    tugas bab deployment

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+// tailwind.config.js
+module.exports = {
+  purge: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+   darkMode: false, // or 'media' or 'class'
+   theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#14b8a6',
+        secondary: '#64748b',
+        dark: '#0f172a'
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
